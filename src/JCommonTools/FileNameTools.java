571b0658e03266946ae93a5e4121b96857138bac
Michael Tor <tormichael@gmail.com>
2015-12-28 11:01:25 +0300

    sync

--- src/JCommonTools/FileNameTools.java
+++ src/JCommonTools/FileNameTools.java
@@ -16,4 +16,23 @@ public class FileNameTools
 		return ret;
 	}
 	
+	public static String ReplaceExtention(String aFileName, String aOldExt, String aNewExt)
+	{
+		String ret = aFileName;
+
+		if (aOldExt != null && aOldExt.length() > 0 && !aOldExt.startsWith("."))
+			aOldExt = "."+aOldExt;
+		if (aNewExt != null && aNewExt.length() > 0 && !aNewExt.startsWith("."))
+			aNewExt = "."+aNewExt;
+
+		if (aOldExt.length()>0 && aFileName.toLowerCase().endsWith(aOldExt))
+		{
+			ret = aFileName.substring(0, aFileName.length() -aOldExt.length() ) + aNewExt;
+		}
+		else
+		{
+			ret = aFileName + aNewExt;
+		}
+		return ret;
+	}
 }
